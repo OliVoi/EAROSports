6b9d081828b59782e4cf8d29f37498992cf88165
hungpopper97@gmail.com <hungpopper97@gmail.com>
2019-11-26 14:21:54 +0700

    update scan device

--- app/src/main/java/com/example/aerosports/MainActivity.kt
+++ app/src/main/java/com/example/aerosports/MainActivity.kt
@@ -1,16 +1,26 @@
 package com.example.aerosports
 
+import android.Manifest
 import android.app.Activity
 import android.bluetooth.BluetoothAdapter
+import android.content.DialogInterface
 import android.content.Intent
+import android.content.pm.PackageManager
+import android.os.Build
 import android.os.Bundle
+import android.text.Html
+import android.text.method.LinkMovementMethod
 import android.view.View
+import android.widget.TextView
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.DialogFragment
 import com.example.aerosports.base.BaseActivity
 import com.example.aerosports.customview.StatusBarView
 import com.example.aerosports.mainui.dialog.ScannerBluetoothDialog
 import com.example.aerosports.utils.StringUtils
 import kotlinx.android.synthetic.main.activity_main.*
+
 
 class MainActivity : BaseActivity(), View.OnClickListener, StatusBarView.CallBackActionBar {
     private val REQUEST_ENABLE_BT: Int = 197
@@ -42,6 +52,22 @@ class MainActivity : BaseActivity(), View.OnClickListener, StatusBarView.CallBac
         return true
     }
 
+    private fun checkPermissions(): Boolean{
+        var isPer: Boolean = false
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) { // Only ask for these permissions on runtime when running Android 6.0 or higher
+            when (ContextCompat.checkSelfPermission(baseContext, Manifest.permission.ACCESS_FINE_LOCATION)) {
+                PackageManager.PERMISSION_DENIED ->{
+                    ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.ACCESS_FINE_LOCATION), 197)
+                    isPer = false
+                }
+                PackageManager.PERMISSION_GRANTED -> {
+                    isPer = true
+                }
+            }
+        }
+        return isPer
+    }
+
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
         if (resultCode != Activity.RESULT_OK) return
@@ -59,7 +85,7 @@ class MainActivity : BaseActivity(), View.OnClickListener, StatusBarView.CallBac
     }
 
     override fun onActionLeft() {
-        if (isFinishing || !checkStatusBluetooth()) return
+        if (isFinishing || !checkStatusBluetooth() || !checkPermissions()) return
         val dialog = ScannerBluetoothDialog()
         dialog.setStyle(DialogFragment.STYLE_NO_TITLE, R.style.ThemeTranslucentNoStatus)
         dialog.show(supportFragmentManager, "Scanning")
--- app/src/main/java/com/example/aerosports/mainui/dialog/ScannerBluetoothDialog.kt
+++ app/src/main/java/com/example/aerosports/mainui/dialog/ScannerBluetoothDialog.kt
@@ -1,5 +1,11 @@
 package com.example.aerosports.mainui.dialog
 
+import android.bluetooth.BluetoothAdapter
+import android.bluetooth.BluetoothDevice
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.content.IntentFilter
 import android.os.Build
 import android.os.Bundle
 import android.view.LayoutInflater
@@ -7,10 +13,28 @@ import android.view.View
 import android.view.ViewGroup
 import android.view.WindowManager
 import androidx.fragment.app.DialogFragment
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.aerosports.R
+import com.example.aerosports.utils.StringUtils
 import kotlinx.android.synthetic.main.dialog_scanner.*
 
-class ScannerBluetoothDialog: DialogFragment(), View.OnClickListener{
+class ScannerBluetoothDialog : DialogFragment(), View.OnClickListener, CallBackDevice {
+    private var mIsScanning = false
+    private var mListDevice: MutableList<BluetoothDevice> = ArrayList()
+    private var mBluetoothAdapter: BluetoothAdapter? = null
+    private var mDeviceAdapter: DeviceBluetoothAdapter? = null
+
+    private val mReceiver: BroadcastReceiver = object : BroadcastReceiver() {
+        override fun onReceive(contextDevice: Context?, intentDevice: Intent?) {
+            when (intentDevice!!.action) {
+                BluetoothDevice.ACTION_FOUND -> {
+                    mListDevice.add(intentDevice!!.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE))
+                    setupViewDevice()
+                }
+            }
+        }
+    }
+
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         var view: View? = inflater?.inflate(R.layout.dialog_scanner, container, false)
         return view!!
@@ -21,9 +45,43 @@ class ScannerBluetoothDialog: DialogFragment(), View.OnClickListener{
         initView()
     }
 
-    fun initView (){
+    private fun initView() {
         btnClose.setOnClickListener(this)
         tvScanning.setOnClickListener(this)
+        scanning()
+    }
+
+    private fun scanning() {
+        mIsScanning = !mIsScanning
+        if (mIsScanning) {
+            tvScanning.text = "Stop"
+            mListDevice = ArrayList()
+            ltAnimation.playAnimation()
+            mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter()
+            mBluetoothAdapter?.startDiscovery()
+            activity?.registerReceiver(mReceiver, IntentFilter(BluetoothDevice.ACTION_FOUND))
+            setupViewDevice()
+            return
+        }
+        tvScanning.text = "Scan"
+        ltAnimation.pauseAnimation()
+        mBluetoothAdapter?.cancelDiscovery()
+        activity?.unregisterReceiver(mReceiver)
+    }
+
+    private fun setupViewDevice(){
+        if (mDeviceAdapter == null){
+            rcDevice.layoutManager = LinearLayoutManager(context)
+            rcDevice.adapter = DeviceBluetoothAdapter(mListDevice, context!!, this)
+            return
+        }
+        mDeviceAdapter?.updateData(mListDevice)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        mBluetoothAdapter?.cancelDiscovery()
+        activity?.unregisterReceiver(mReceiver)
     }
 
     override fun onStart() {
@@ -36,9 +94,13 @@ class ScannerBluetoothDialog: DialogFragment(), View.OnClickListener{
     }
 
     override fun onClick(v: View?) {
-        when(v?.id){
+        when (v?.id) {
             R.id.btnClose -> dismiss()
-            R.id.tvScanning -> dismiss()
+            R.id.tvScanning -> scanning()
         }
+    }
+
+    override fun onclickItem(item: BluetoothDevice) {
+
     }
 }
